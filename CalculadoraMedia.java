9f3aec5d7a024770b4d233a605b1941be28e9f29
Fernando Angiolin <fernandoangiolin.contato@gmail.com>
2026-05-20 21:47:38 -0300

    Adiciona calculo da media e resultado final: Fernando

--- CalculadoraMedia.java
+++ CalculadoraMedia.java
@@ -1,15 +1,25 @@
-
 import java.util.Scanner;
 
 public class CalculadoraMedia {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nome do aluno");
+        
+        System.out.print("Nome do aluno: ");
         String nome = sc.nextLine();
-        System.out.println("Informe a nota da primeira prova");
-        int nota1 = sc.nextInt();
-        System.out.println("Informe a nota da segunda prova");
-        int nota2 = sc.nextInt();
+
+        System.out.print("Informe a nota da primeira prova: ");
+        float nota1 = sc.nextFloat();
+
+        System.out.print("Informe a nota da segunda prova: ");
+        float nota2 = sc.nextFloat();
+
+        double media = (nota1 + nota2) / 2.0;
+
+        if (media >= 7) {
+            System.out.println("Aluno " + nome + " foi aprovado com média " + media);
+        } else {
+            System.out.println("Aluno " + nome + " foi reprovado com média " + media);
+        }
     }
 }
